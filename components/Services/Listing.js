3f8cec84690a22c2795093a9699c2c2b4bf0f5df
Mutesi <ktesialine@gmail.com>
2022-10-26 20:14:25 +0200

    update on structure

--- components/Services/Listing.js
+++ components/Services/Listing.js
@@ -1,12 +1,11 @@
-
 import CardList from "../layout/CardList";
-export default function Listing(){
+export default function Listing() {
   return (
-    <div className="flex  relative gap-10 pt-[60px] pl-[120px] pr-[138px] pb-[120px]">
-       <div className="flex flex-col gap-[38px] w-4/5">
+    <div className="flex relative gap-10 pt-[60px] pl-[120px] pr-[138px] pb-[120px]">
+      <div className="flex flex-col gap-[38px] w-4/5">
         <img src="gram2.png" />
       </div>
-      <CardList/>
+      <CardList />
     </div>
   );
 }
